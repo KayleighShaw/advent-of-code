b78e22aecf266d73c3683d293728ed83e9a9cbae
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-12-06 15:00:19 +0000

    Day 5, part 1: code working with test input

--- day5/day5.go
+++ day5/day5.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"regexp"
@@ -78,15 +79,68 @@ func makeGrid(maxCoord int) [][]int {
 }
 
 func markGrid(grid [][]int, move Movement) [][]int {
+	incrementer := 1
+	fmt.Printf("sX: %d, eX: %d, sY: %d, eY: %d\n", move.StartX, move.EndX, move.StartY, move.EndY)
 
+	if move.StartX > move.EndX || move.StartY > move.EndY {
+		incrementer = -1
+	}
+
+	if move.StartX == move.EndX {
+		// movement along y axis
+		for y := move.StartY; ; {
+			lastRun := false
+			if y == move.EndY {
+				lastRun = true
+			}
+			grid[move.StartX][y] += 1
+			fmt.Printf("Marking: %d,%d\n", move.StartX, y)
+			y += incrementer
+			if lastRun {
+				break
+			}
+		}
+	} else if move.StartY == move.EndY {
+		// movement along x axis
+		for x := move.StartX; ; {
+			lastRun := false
+			if x == move.EndX {
+				lastRun = true
+			}
+			grid[x][move.StartY] += 1
+			fmt.Printf("Marking: %d,%d\n", x, move.StartY)
+			x += incrementer
+			if lastRun {
+				break
+			}
+		}
+	} else {
+		// diagonal movement
+		fmt.Println("Diagonal movement")
+	}
+
+	return grid
 }
 
 func main() {
 	moveList, maxCoord := getMoveList()
-	grid := makeGrid(maxCoord)
+	grid := makeGrid(maxCoord + 1)
 
 	for _, m := range moveList {
 		grid = markGrid(grid, m)
 	}
 
+	var count int
+
+	for i := 0; i <= maxCoord; i++ {
+		for j := 0; j <= maxCoord; j++ {
+			fmt.Print(grid[i][j])
+			fmt.Print(" ")
+			if grid[i][j] > 1 {
+				count++
+			}
+		}
+		fmt.Println("")
+	}
+	fmt.Printf("The count is: %d", count)
 }
