c6c388dae7602b9be6713f7cdaa39145ed799440
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-12-06 18:33:55 +0000

    Day 6, part 2 completed :D

--- day6/day6.go
+++ day6/day6.go
@@ -10,14 +10,13 @@ import (
 )
 
 func getDataArray() []int {
-	// file, err := os.Open("input.txt")
-	file, err := os.Open("day6test.txt")
+	file, err := os.Open("input.txt")
+	// file, err := os.Open("day6test.txt")
 	if err != nil {
 		log.Fatalf("Failed to open file")
 	}
 
 	scanner := bufio.NewScanner(file)
-	// scanner.Split(bufio.ScanLines)
 	var text []string
 
 	for scanner.Scan() {
@@ -37,38 +36,47 @@ func getDataArray() []int {
 	return intArray
 }
 
-func main() {
-	fishArray := getDataArray()
+func generateAgeTracker(fish []int) [9]int {
+	var ageTracker [9]int
 
-	// fmt.Println(fishArray)
+	for _, f := range fish {
+		ageTracker[f]++
+	}
+	return ageTracker
+}
 
-	// var babyFish []int //make this a slice
-	babyFish := make([]int, 0)
-	newFishiesList := make([]int, 0)
+func sumAgeList(ageList [9]int) int {
+	var sum int
 
-	for i := 0; i < 80; i++ {
-		for _, f := range fishArray {
-			// fmt.Println(f)
-			switch {
-			case f == 0:
-				newFishiesList = append(newFishiesList, 6)
-				babyFish = append(babyFish, 8)
-			default:
-				f--
-				newFishiesList = append(newFishiesList, f)
-			}
-			// newFishiesList = append(newFishiesList, babyFish...)
-			// fmt.Print("New Fish List: ")
-			// fmt.Println(newFishiesList)
-			// fmt.Print("Baby Fish: ")
-			// fmt.Println(babyFish)
-		}
-		fishArray = newFishiesList
-		fishArray = append(fishArray, babyFish...)
-		// fmt.Printf("Day %d: %v\n", i+1, fishArray)
-		babyFish = nil
-		newFishiesList = nil
+	for _, c := range ageList {
+		sum += c
 	}
 
-	fmt.Printf("The number of fishies is: %d", len(fishArray))
+	return sum
+}
+
+func main() {
+	fishArray := getDataArray()
+	ageList := generateAgeTracker(fishArray)
+	fmt.Println(ageList)
+
+	// iterate through days
+	for i := 0; i < 256; i++ {
+		var updatedAgeList [9]int
+
+		// iterate through ageList
+		for j, f := range ageList {
+			switch {
+			case j == 0:
+				updatedAgeList[6] = f
+				updatedAgeList[8] = f
+			default:
+				updatedAgeList[j-1] += f
+			}
+		}
+		ageList = updatedAgeList
+	}
+
+	count := sumAgeList(ageList)
+	fmt.Printf("There are %d fish!", count)
 }
