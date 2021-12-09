d129292675f6d24649c0dd844693bf78f74a3e81
Kayleigh Shaw <kayleigh.shaw@infinityworks.com>
2021-12-09 01:00:10 +0000

    Day 8, part 1 completed

--- day8/day8.go
+++ day8/day8.go
@@ -8,35 +8,78 @@ import (
 	"strings"
 )
 
-func getDataArray() []string {
-	// file, err := os.Open("input.txt")
-	file, err := os.Open("day8test.txt")
+func removeEmpty(in []string, remove string) []string {
+	out := make([]string, 0)
+
+	for _, s := range in {
+		if s != remove {
+			out = append(out, s)
+		}
+	}
+
+	return out
+}
+
+func makeInputOutputArrays(input []string, output []string) ([]string, []string) {
+	inputArray := removeEmpty(input, "")
+	outArray := removeEmpty(output, "")
+
+	return inputArray, outArray
+}
+
+func getDataArray() [][]string {
+	file, err := os.Open("input.txt")
+	// file, err := os.Open("day8test.txt")
 	if err != nil {
 		log.Fatalf("Failed to open file")
 	}
 
-	trimmedArray := make([]string, 0)
-
+	mainArray := make([][]string, 0)
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		newLine := strings.Split(line, "|")
-
-		for _, l := range newLine {
-			l = strings.Trim(l, " ")
-			trimmedArray = append(trimmedArray, l)
-		}
+		input := strings.Split(newLine[0], " ")  // one line array
+		output := strings.Split(newLine[1], " ") // one line array
+		inputArray, outputArray := makeInputOutputArrays(input, output)
+		mainArray = append(mainArray, inputArray)
+		mainArray = append(mainArray, outputArray)
 	}
 
+	// fmt.Print(mainArray[0])
+
 	file.Close()
-	return trimmedArray
+	return mainArray
 }
 
 func main() {
 	array := getDataArray()
-	fmt.Println(array[0])
-	fmt.Println(array[1])
-	fmt.Println(array[2])
-	fmt.Println(array[3])
+	fmt.Println(array)
+	// fmt.Println(array[1])
+	// fmt.Println(array[2])
+	// fmt.Println(array[3])
+
+	// newArray := [4]string{"cg", "cg", "fdcagb", "cbg"}
+
+	count := 0
+
+	for i := 1; i < len(array); i += 2 {
+		for j := 0; j < len(array[i]); j++ {
+			switch {
+			case len(array[i][j]) == 2:
+				count++
+			case len(array[i][j]) == 4:
+				count++
+			case len(array[i][j]) == 3:
+				count++
+			case len(array[i][j]) == 7:
+				count++
+			default:
+				continue
+			}
+		}
+	}
+
+	fmt.Println(count)
 }
